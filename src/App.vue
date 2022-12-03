36a043395cff7be3e2bfe2d403a66736b12a27f0
hoo <sihookang47@gmail.com>
2022-12-03 18:38:29 +0900

    Feat: solve day1 homework

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>원룸 샵</h1>
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>{{dic[products[0]]+'만원'}}</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{dic[products[1]]}}</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{dic[products[2]]}}</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      products : ['역삼동원룸', '천호동원룸', '상도동원룸'],
+      dic: {
+        역삼동원룸: '50',
+        천호동원룸: '가격은 아무거나',
+        상도동원룸: '가격은 아무거나',
+      }
+    }
+  },
+  components: {}
 }
 </script>
 
