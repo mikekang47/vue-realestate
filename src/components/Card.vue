72b16d3ae348ca44b1016eb3d43f2a86e9a31ecf
hoo <sihookang47@gmail.com>
2022-12-04 17:58:54 +0900

    Feat: add event handler

---
+++ src/components/Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img @click="$emit('openModal'); $emit('modifyRoom', room)" :src="room.image" class="room-img">
+    <h4 @click="$emit('openModal'); $emit('modifyRoom', room)">{{ room.title }}</h4>
+    <p>{{ room.price }}</p>
+  </div>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Card',
+  props: {
+    idx: Number,
+    room: {
+      image: String,
+      title: String,
+      price: Number
+    },
+  }
+}
+
+</script>
+<style>
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+</style>
