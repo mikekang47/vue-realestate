480db5bc17bb3d90315971bee571f65d69923d8c
mikekang47 <gyeong0308@gmail.com>
2022-12-12 16:12:26 +0900

    Feat: 문자열 예외 추가

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,8 +3,8 @@
     <div class="white-bg">
       <h4>{{ room.title }}</h4>
       <p>{{ room.content }}</p>
-      <input v-model="month">
-      <p>{{month}} 개월 선택함: {{month * room.price}} 원</p>
+      <input v-model.number="month">
+      <p>{{ month }} 개월 선택함: {{ month * room.price }} 원</p>
       <button @click="$emit('closeModal')">X</button>
     </div>
   </div>
@@ -15,6 +15,20 @@ export default {
   data() {
     return {
       month: 1,
+    }
+  },
+  watch: {
+    // 파라마터 첫번째 인자 -> 변경 후 , 두 번째 인자 -> 변경 전 인자
+    month(input) {
+      if(input > 13) {
+        alert("13개월 이상 입력할 수 없습니다.");
+        this.month = 1
+      }
+      const reg = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-z]/;
+      if(reg.test(input)) {
+        alert("문자는 입력 불가능 합니다.");
+        this.month = 1
+      }
     }
   },
   props: {
